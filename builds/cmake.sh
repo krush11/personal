1bd3286dbb4cfca2daf34bd0e1c25c2af68435df
krush11 <krushnalpatel11@gmail.com>
2021-03-30 17:16:27 +0530

    update cmake installation script

--- builds/cmake.sh
+++ builds/cmake.sh
@@ -4,12 +4,22 @@ export version=3.19
 export build=3
 mkdir ~/temp
 cd ~/temp
+# Download cmake source
 wget https://cmake.org/files/v$version/cmake-$version.$build.tar.gz
 tar -xzvf cmake-$version.$build.tar.gz
 cd cmake-$version.$build/
+
+# Initiate build
 ./bootstrap
+
+# Build cmake and install
 make -j$(nproc)
 sudo make install
+
+# Remove downloaded source
 cd ~
 rm -rf temp
+
+# Copy latest cmake to binaries
+cp /usr/local/bin/cmake /usr/bin/cmake
 cmake --version
